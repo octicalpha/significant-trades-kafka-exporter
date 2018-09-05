15a6c62aa18e84cb8fa27aedc31616c0fdfdd775
Dmitry Evdokimov <devdokimov@alfabank.ru>
2018-09-06 01:34:06 +0300

    fix: kafka port

--- run_docker.sh
+++ run_docker.sh
@@ -3,7 +3,7 @@
 docker rmi $(docker images -q | grep stke)
 docker run --rm --name=stke \
   -e "LOG_LEVEL=INFO" \
-  -e "KAFKA_URL=localhost:3000" \
+  -e "KAFKA_URL=localhost:9092" \
   -e "WS_URLS=ws://localhost:10000,ws://localhost:10001" \
   -e "PAIR_LIST=BTC/USD,ETH/USD" \
   dmi7ry/stke:latest
