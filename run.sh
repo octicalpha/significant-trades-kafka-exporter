0f61e3fb6f15cc5c7ed05fae15f1730b47741c7a
Dmitry Evdokimov <devdokimov@alfabank.ru>
2018-09-05 23:09:03 +0300

    fix: test ports

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 export LOG_LEVEL=INFO
 export KAFKA_URL="localhost"
-export WS_URLS="ws://localhost:3000,ws://localhost:3001"
+export WS_URLS="ws://localhost:10000,ws://localhost:10001"
 export PAIR_LIST="BTC/USD,ETH/USD"
 set -e
 python3 -m src
