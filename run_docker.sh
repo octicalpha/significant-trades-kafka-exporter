a35771fd9a8a552b954867275a235425d1af84c1
Dmitry Evdokimov <devdokimov@alfabank.ru>
2018-09-07 01:11:15 +0300

    fix: docker run sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 docker rmi $(docker images | grep stke | awk '{ print $1 }')
-docker run --rm --name=stke \
+docker run --rm \
+  --name=stke \
   --network=host \
+  -v /var/run/docker.sock:/var/run/docker.sock:ro \
   -e "LOG_LEVEL=INFO" \
   -e "PAIR_LIST=BTC/USD,ETH/USD" \
   dmi7ry/stke:latest
